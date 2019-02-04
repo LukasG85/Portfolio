51f09e0ad155e2172e25589d368b84da5d909c5c
LukasG85 <lukas85b@gmail.com>
2019-02-04 13:23:52 +0100

    portfolio v1

--- src/pages/work.js
+++ src/pages/work.js
@@ -38,55 +38,11 @@ export default class work extends Component {
               render={data => {
                 const products = data.items.edges;
                 return products.map(item => {
-                  return (
-                    <Project key={item.node.id} product={item.node} />
-                    // <div key={item.node.id} className="item">
-                    //   <a
-                    //     className="image"
-                    //     href={item.node.url}
-                    //     target="_blank"
-                    //     rel="noopener noreferrer"
-                    //   >
-                    //     <p>{item.node.title}</p>
-                    //     <Img fixed={item.node.img.fixed.src} alt="Project" />
-                    //   </a>
-                    /* <a href="#" className="btn-light">
-                        <i className="fas fa-eye">Project</i>
-                      </a> */
-                    /* <a
-                        href="https://github.com/LukasG85/WeatherApp"
-                        className="btn-dark"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fab fa-eye"> Github</i>
-                      </a> */
-                    // </div>
-                  );
-                  // <Product key={item.node.id} product={item.node} />
+                  return <Project key={item.node.id} product={item.node} />;
                 });
               }}
             />
           </div>
-          {/* <div className="projects">
-            <div className="item">
-              <a className="image" href="#">
-                <p>Weather</p>
-                <img src="./img/WeatherApp.jpg" alt="Project">
-              </a>
-               <a href="#" class="btn-light">
-                      <i class="fas fa-eye">Project</i>
-                  </a>
-               <a
-                href="https://github.com/LukasG85/WeatherApp"
-                className="btn-dark"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-eye"> Github</i>
-              </a>
-            </div>
-          </div> */}
         </main>
       </Layout>
     );
