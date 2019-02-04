508480a9b497324e99e241757ab32c39192d511d
LukasG85 <lukas85b@gmail.com>
2019-02-04 18:17:50 +0100

    portfolio v3

--- src/components/global/Typing.js
+++ src/components/global/Typing.js
@@ -1,22 +1,117 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React, { Component } from "react";
 
-function Typing() {
-  return (
-    <>
-      <Typewriter
-        options={{
-          strings: [
-            `Hi, I'm a web developer`,
-            `I create websites`,
-            `And mobile applications`
-          ],
-          autoStart: true,
-          loop: true
-        }}
-      />
-    </>
-  );
+export default class Typing extends Component {
+  state = {
+    active: false
+  };
+
+  componentDidMount() {
+    //Typing
+    const spnText = document.querySelector(".text");
+
+    const txt = [
+      `Hi, I'm a web developer`,
+      `I create websites`,
+      `And mobile applications`
+    ];
+
+    let acttiveLetter = -10;
+    let activeWord = 0;
+
+    const addLetter = () => {
+      if (acttiveLetter >= 0) {
+        spnText.textContent += txt[activeWord][acttiveLetter];
+      }
+
+      acttiveLetter++;
+
+      if (acttiveLetter === txt[activeWord].length) {
+        activeWord++;
+        if (activeWord === txt.length) {
+          activeWord = 0;
+        }
+
+        return setTimeout(() => {
+          acttiveLetter = 0;
+          spnText.textContent = "";
+          addLetter();
+        }, 2000);
+      }
+
+      setTimeout(addLetter, 100);
+    };
+
+    addLetter();
+
+    const cursorAnimation = () => {
+      this.setState({
+        active: !this.state.active
+      });
+    };
+    setInterval(cursorAnimation, 400);
+  }
+
+  render() {
+    return (
+      <h2 className="sm-heading">
+        <span className="text" />
+        <span className="cursor">|</span>
+      </h2>
+    );
+  }
 }
 
-export default Typing;
+// import React from "react";
+
+// //Typing
+// const spnText = document.querySelector(".text");
+// const spnCursor = document.querySelector(".cursor");
+// const txt = [
+//   `Hi, I'm a web developer`,
+//   `I create websites`,
+//   `And mobile applications`
+// ];
+// console.log(spnCursor, spnText);
+// let acttiveLetter = -10;
+// let activeWord = 0;
+
+// const addLetter = () => {
+//   if (acttiveLetter >= 0) {
+//     spnText.textContent += txt[activeWord][acttiveLetter];
+//   }
+
+//   acttiveLetter++;
+
+//   if (acttiveLetter === txt[activeWord].length) {
+//     activeWord++;
+//     if (activeWord === txt.length) {
+//       activeWord = 0;
+//     }
+
+//     return setTimeout(() => {
+//       acttiveLetter = 0;
+//       spnText.textContent = "";
+//       addLetter();
+//     }, 2000);
+//   }
+
+//   setTimeout(addLetter, 100);
+// };
+
+// addLetter();
+
+// const cursorAnimation = () => {
+//   spnCursor.classList.toggle("active");
+// };
+// setInterval(cursorAnimation, 400);
+
+// function Typing() {
+//   return (
+//     <h2 className="sm-heading">
+//       <span className="text">{}</span>
+//       <span className="cursor">|</span>
+//     </h2>
+//   );
+// }
+
+// export default Typing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Icons from "../components/global/SocialIcons";
-// import Typing from "../components/global/Typing";
+import Typing from "../components/global/Typing";
 
 const IndexPage = () => (
   <Layout>
@@ -11,7 +11,7 @@ const IndexPage = () => (
       <h1 className="lg-heading">
         Łukasz <span className="text-secondary">Gancarz</span>
       </h1>
-      <h2 className="sm-heading">{/* <Typing /> */}</h2>
+      <Typing />
       <div className="icons">
         <Icons />
       </div>
